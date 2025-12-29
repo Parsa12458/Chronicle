9b6843f858a7e93796a66e12e87a3551162d3904
Parsa12458 <parsa.shirafkan127@gmail.com>
2025-12-29 13:00:04 +0330

    connect auth.js with supabase using adapter

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,14 +1,26 @@
+import { SupabaseAdapter } from "@auth/supabase-adapter";
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: SupabaseAdapter({
+    url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    secret: process.env.SUPABASE_SERVICE_ROLE_KEY,
+  }),
   providers: [Google],
   callbacks: {
-    authorized: async ({ auth }) => {
+    authorized() {
       return !!auth;
+    },
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
     },
   },
   pages: {
     signIn: "/login",
   },
+  debug: true,
 });
